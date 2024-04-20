7f3ce3c252e75c3313d47573fad091c2e3af9706
Peyman GhaderKourehpaz <poyeshsoftware@gmail.com>
2024-04-20 22:31:36 +0200

    update vite

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,12 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        outDir: 'dist/',
-        assetsDir: 'assets/',
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                router: '/src/router/index.js',
-                store: '/src/stores/index.js'
-            }
-        }
-    },
+    base: '/Einbuergerungstest/',
     plugins: [vue()],
     resolve: {
         alias: {
             '@': '/src'
         }
     },
-    base: '/Einbuergerungstest/'
+
 })
