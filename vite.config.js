86afa7cde66acda9a310db21bf4c9e73d6cd39b1
Peyman GhaderKourehpaz <poyeshsoftware@gmail.com>
2024-04-20 22:17:20 +0200

    update index.html and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         outDir: 'dist/',
-        assetsDir: 'assets/'
+        assetsDir: 'assets/',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                router: '/src/router/index.js',
+                store: '/src/stores/index.js'
+            }
+        }
     },
     plugins: [vue()],
     resolve: {
